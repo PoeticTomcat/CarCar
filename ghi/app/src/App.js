6b25df3cc1c2b30023e998475b656e24e6bb8b49
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-23 18:05:45 -0400

    Updated Routes in App.js to reflect vehicle model form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
+import Nav from "./Nav";
+import ManufacturerForm from "./ManufacturerForm";
+import AutomobileForm from "./AutomobileForm";
+import VehicleModelForm from "./VehicleModelForm";
 
 function App() {
   return (
@@ -9,6 +12,11 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="inventory">
+            <Route path="manufacturers/new" element={<ManufacturerForm />} />
+            <Route path="vehicles/new" element={<VehicleModelForm />} />
+            <Route path="automobiles/new" element={<AutomobileForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
