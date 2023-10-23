3aeae26e150bf9d41b1b1fd203ca130e315fcc94
Seth Porche <porchest2022@outlook.com>
2023-10-23 18:30:40 -0400

    Ramshackled some JSX together for AutomobileForm ROUND 2

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
-// import ManufacturerForm from './inventory/ManufacturerForm';
-import AutomobileForm from './AutomobileForm';
-import VehicleModelForm from './VehicleModelForm';
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
 import Nav from "./Nav";
 import AutomobileForm from "./inventory/AutomobileForm";
 import VehicleModelForm from "./inventory/VehicleModelForm";
->>>>>>> d17da385ad24c8abfc893d69e3af2dae68b390ef
 
 function App() {
   return (
@@ -20,19 +11,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-<<<<<<< HEAD
-          <Route path="inventory">
-              {/* <Route path="manufacturers" element={<ManufacturerList />} />
-              <Route path="manufacturers/new" element={<ManufacturerForm />} /> */}
-              {/* <Route path="vehicles" element={<VehicleModelList />} /> */}
-              <Route path="vehicles/new" element={<VehicleModelForm />} />
-              {/* <Route path="automobiles" element={<AutomobileList />} /> */}
-              <Route path="automobiles/new" element={<AutomobileForm />} />
-              </Route>
-=======
           <Route path="models/new/" element={<VehicleModelForm />} />
           <Route path="automobiles/new/" element={<AutomobileForm />} />
->>>>>>> d17da385ad24c8abfc893d69e3af2dae68b390ef
         </Routes>
       </div>
     </BrowserRouter>
