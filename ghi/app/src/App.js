747dbdf6a5c1998782b4ea560a5964fc01499082
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-23 19:10:32 -0400

    Created VehicleModelList.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -3,6 +3,7 @@ import MainPage from "./MainPage";
 import Nav from "./Nav";
 import AutomobileForm from "./inventory/AutomobileForm";
 import VehicleModelForm from "./inventory/VehicleModelForm";
+import VehicleModelList from "./inventory/VehicleModelList";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="models/new/" element={<VehicleModelForm />} />
+          <Route path="models/" element={<VehicleModelList />} />
           <Route path="automobiles/new/" element={<AutomobileForm />} />
         </Routes>
       </div>
---
+++ ghi/app/src/inventory/VehicleModelList.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+
+function VehicleModelList() {
+  const [models, setModels] = useState([]);
+  const fetchData = async () => {
+    const url = "http://localhost:8100/api/models/";
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setModels(data.models);
+    }
+  };
+
+  async function handleClick(modelId) {
+    const response = await fetch(
+      `http://localhost:8100/api/models/${modelId}`,
+      {
+        method: "DELETE",
+      }
+    );
+    if (response.ok) {
+      alert("Delete Item");
+      fetchData();
+    } else {
+      alert("Could Not Delete Item");
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!models) {
+    return <div>Loading models...</div>;
+  }
+
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Manufacturer</th>
+          <th>Picture</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {models.map((model, index) => {
+          return (
+            <tr key={index}>
+              <td>{model.name}</td>
+              <td>{model.manufacturer.name}</td>
+              <td>
+                <img
+                  src={model.picture_url}
+                  className="img-thumbnail"
+                  style={{ height: "70px", width: "70px" }}
+                />
+              </td>
+              <td>
+                <button
+                  onClick={() => handleClick(model.id)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default VehicleModelList;
