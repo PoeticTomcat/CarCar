4e28d6b2466f7dfb51a8c424f94a67414ef63ab6
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-24 18:52:06 -0400

    Set up poller

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -10,17 +10,38 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service_project.settings")
 django.setup()
 
 # Import models from service_rest, here. Ignore vs-code error hinting
-# from service_rest.models import Something
+from service_rest.models import AutomobileVO
+
+def get_auto_vo():
+    url = "http://localhost:8100/api/automobiles/"
+    response = requests.get(url)
+    content = json.loads(response.content)
+    for auto in content["automobiles"]:
+        print(auto)
+        try:
+            obj, created = AutomobileVO.objects.update_or_create(
+                vin=auto["vin"],
+                defaults={
+                    "vin": auto["vin"],
+                    "sold": auto["sold"],
+                    "id": auto["id"]
+                }
+            )
+            if created:
+                print("Created an auto VO object", obj)
+            else:
+                print("Updated, obj")
+        except:
+            print("Did not create nor update an auto vo")
 
 
 def poll(repeat=True):
     while True:
         print('Service poller polling for data')
         try:
-            # Write your polling logic, here
-            # Do not copy entire file
+            get_auto_vo()
             pass
-        
+
         except Exception as e:
             print(e, file=sys.stderr)
 
