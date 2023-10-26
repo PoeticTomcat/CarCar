029d544ee6152b01fcd9e3da9b1e18bcd1c7d3d5
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-10-26 11:45:27 -0400

    Added AppointmentList.js and AppointmentForm.js,  Both visually correct, but missing is_vip field and haven't tested functionality of buttons

--- ghi/app/src/service/AppointmentHistory.js
+++ ghi/app/src/service/AppointmentHistory.js
@@ -1,5 +1,73 @@
 import React, { useEffect, useState } from "react";
 
 function AppointmentHistory() {
+    const [appointments, setApointments] = useState([]);
+  const fetchData = async () => {
+    const url = "http://localhost:8080/api/appointments/";
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setApointments(data.appointments);
+    }
+  };
+
+  async function handleClick(id) {
+    const response = await fetch(
+      `http://localhost:8080/api/appointments/${id}/`,
+      {
+        method: "PUT",
+      }
+    );
+    if (response.ok) {
+      alert("Updated appointment");
+      fetchData();
+    } else {
+      alert("Could not update appointment");
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!appointments) {
+    return <div>Loading appointments...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Appointment History</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Vin</th>
+            <th>Is VIP?</th>
+            <th>Customer</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Technician</th>
+            <th>Reason</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map((appointment, index) => {
+            return (
+              <tr key={index}>
+                <td>{appointment.vin}</td>
+                <td>{appointment.sold}</td>
+                <td>{appointment.customer}</td>
+                <td>{new Date(appointment.date_time).toLocaleDateString()}</td>
+                <td>{new Date(appointment.date_time).toLocaleTimeString()}</td>
+                <td>{appointment.technician.first_name} {appointment.technician.last_name}</td>
+                <td>{appointment.reason}</td>
+                <td>{appointment.status}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default AppointmentHistory
--- ghi/app/src/service/AppointmentList.js
+++ ghi/app/src/service/AppointmentList.js
@@ -1,5 +1,86 @@
 import React, { useEffect, useState } from "react";
 
 function AppointmentList() {
+  const [appointments, setApointments] = useState([]);
+  const fetchData = async () => {
+    const url = "http://localhost:8080/api/appointments/";
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setApointments(data.appointments);
+    }
+  };
+
+  async function handleClick(id) {
+    const response = await fetch(
+      `http://localhost:8080/api/appointments/${id}/`,
+      {
+        method: "PUT",
+      }
+    );
+    if (response.ok) {
+      alert("Updated appointment");
+      fetchData();
+    } else {
+      alert("Could not update appointment");
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!appointments) {
+    return <div>Loading appointments...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Appointments</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Vin</th>
+            <th>Is VIP?</th>
+            <th>Customer</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Technician</th>
+            <th>Reason</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map((appointment, index) => {
+            return (
+              <tr key={index}>
+                <td>{appointment.vin}</td>
+                <td>{appointment.sold}</td>
+                <td>{appointment.customer}</td>
+                <td>{new Date(appointment.date_time).toLocaleDateString()}</td>
+                <td>{new Date(appointment.date_time).toLocaleTimeString()}</td>
+                <td>{appointment.technician.first_name} {appointment.technician.last_name}</td>
+                <td>{appointment.reason}</td>
+                <td>
+                  <button
+                    onClick={() => handleClick(appointment.id)}
+                    className="btn btn-danger"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => handleClick(appointment.id)}
+                    className="btn btn-success"
+                  >
+                    Finish
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-export default AppointmentList
+export default AppointmentList;
