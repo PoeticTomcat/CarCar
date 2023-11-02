f80e21d031af601e99232ec2b8c80a757b9d9fd0
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-11-02 14:03:26 -0400

    Ready for presentation

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -12,23 +12,23 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/manufacturers/">Manufacturers</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/manufacturers/new/">+</NavLink></li>
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/manufacturers/new/">+</NavLink></li> */}
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/models/">Models</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/models/new/">+</NavLink></li>
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/models/new/">+</NavLink></li> */}
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/automobiles/">Automobiles</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/automobiles/new/">+</NavLink></li>
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/automobiles/new/">+</NavLink></li> */}
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/technicians/">Technicians</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/technicians/new/">+</NavLink></li>
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/technicians/new/">+</NavLink></li> */}
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/">Appointments</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/new/">+</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/history/">Appt history</NavLink></li>
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/new/">+</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/history/">Appt history</NavLink></li> */}
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/salespeople/">Salespeople</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/salespeople/new/">+</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/sales/history/">Sales history</NavLink></li>
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/salespeople/new/">+</NavLink></li> */}
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/sales/history/">Sales history</NavLink></li> */}
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/customers/">Customers</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/customers/new/">+</NavLink></li>
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/customers/new/">+</NavLink></li> */}
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/sales/">Sales</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/sales/new/">+</NavLink></li>
+            {/* <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/sales/new/">+</NavLink></li> */}
           </ul>
         </div>
       </div>
