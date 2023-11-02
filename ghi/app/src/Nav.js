69bb8a91b0e752ae183922f226706cfe0ae4486c
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-11-02 11:07:31 -0400

    Final navbar updates

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -5,9 +5,9 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" aria-current="page" to="/">CarCar</NavLink>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -15,10 +15,8 @@ function Nav() {
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/models/">Models</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/automobiles/">Automobiles</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/technicians/" >Technicians</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/">Open appointments</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/history/">Service History</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/appointments/">Appointments</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/salespeople/">Salespeople</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/sales/history/" >Salesperson History</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/customers/">Customers</NavLink></li>
             <li className="nav-item"><NavLink className="nav-link" aria-current="page" to="/sales/">Sales</NavLink></li>
           </ul>
